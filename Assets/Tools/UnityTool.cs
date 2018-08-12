6c2d7c3a2df0d36fe79334e2bda7b777d125c1d3
RosaTs <1161814672@qq.com>
2018-08-12 23:56:22 +0800

    添加2DUI跟随3D物体

--- Assets/Tools/UnityTool.cs
+++ Assets/Tools/UnityTool.cs
@@ -6,6 +6,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Text;
 
 public class UnityTool : MonoBehaviour {
 
@@ -38,6 +40,61 @@ public class UnityTool : MonoBehaviour {
         return obj;
     }
 
-    
+
+
+    /// <summary>
+    /// 读取json返回string类型的内容
+    /// </summary>
+    /// <param name="jsonFileName">文件名</param>
+    public static string ReadJsonToString(string jsonFileName)
+    {
+        if (!Directory.Exists("JsonFile"))
+        {
+            Directory.CreateDirectory("JsonFile");  //创建文件夹
+        }
+        if (File.Exists("JsonFile/" + jsonFileName + ".json"))
+        {
+            FileInfo fileInfo = new FileInfo("JsonFile/" + jsonFileName + ".json");
+            StreamReader streamReader = fileInfo.OpenText();
+            Debug.Log("Json文件读取成功");
+            string str = streamReader.ReadToEnd();
+            streamReader.Close();               //文件流释放
+            streamReader.Dispose();
+            return str;
+        }
+        else
+        {
+            Debug.Log("路径或名字错误！！");
+            return default(string);
+        }
+    }
+
+    /// <summary>
+    ///写入Json数据到文件
+    /// </summary>
+    /// <param name="jsonFileName"></param>
+    /// <param name="data"></param>
+    public static void WriteStringToJson(string jsonFileName, string data)
+    {
+        if (!Directory.Exists("JsonFile"))
+        {
+            Directory.CreateDirectory("JsonFile");  //创建文件夹
+        }
+        if (File.Exists("JsonFile/" + jsonFileName + ".json"))
+        {
+            Debug.Log("有文件修改成功");
+            File.WriteAllText("JsonFile/" + jsonFileName + ".json", data);
+        }
+        else
+        {
+            FileStream fs = new FileStream("JsonFile/" + jsonFileName + ".json", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter m_streamWriter = new StreamWriter(fs,Encoding.UTF8);   //UTF8格式写入
+            m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
+            m_streamWriter.WriteLine(data);
+            m_streamWriter.Flush();
+            m_streamWriter.Close();
+        }
+    }
+
 
 }
