3fa1fe1a86a44337197f46d55f20a50358a573f2
雪狐Monster <1161814672@qq.com>
2018-09-24 15:47:15 +0800

    增加简易UI动画模块

---
+++ Assets/UnityAssets_YU/EasyAnimation/Scripts/Moulds/EasyAnimation_Enlarge.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace EasyAnimation {
+
+    [AddComponentMenu("EasyAnimation/缩放效果")]
+    public class EasyAnimation_Enlarge : EasyAnimationTemplateMethod
+    {
+
+        RectTransform rectTransform;
+
+        Vector3 rectSize = Vector3.one;
+
+        protected override void Easy_Animation_Awake()
+        {
+            rectTransform = transform as RectTransform;
+            rectSize = rectTransform.localScale;
+        }
+
+        protected override void PrimitiveOperation_Start()
+        {
+            ead = new EaseAinmationDrive(1, 0, 1, easetype);
+        }
+
+        protected override bool PrimitiveOperation_UpDate(float time)
+        {
+            rectTransform.localScale = rectSize * ead.getProgress(time) ;
+            return true;
+        }
+
+        public override void Rese()
+        {
+            rectTransform.localScale = initScale;
+        }
+    }
+}
